819a4d54a3f8355bb65e444e584221e930490ddb
neerajp99 <neerajp1999@gmail.com>
2019-06-17 23:03:14 +0530

    Edit tests for Settings

--- Unit/SettingsTest.php
+++ Unit/SettingsTest.php
@@ -1,66 +1,40 @@
 <?php
 
 use Drupal\Tests\UnitTestCase;
-use PHPUnit\Framework\TestCase;
 use Drupal\social_auth\Settings\SettingsBase;
-use Drupal\social_api\Settings\SettingsBase as SocialApiSettingsBase;
 use Drupal\Core\Config\ImmutableConfig;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Drupal\Core\Config\StorageInterface;
 use Drupal\Core\Config\TypedConfigManagerInterface;
-use Drupal\social_auth\Settings\SettingsInterface;
-use Drupal\social_api\Settings\SettingsInterface as SettingsInterfaceBase;
 
-
+/**
+ * Define Test Class SettingsTest.
+ */
 class SettingsTest extends UnitTestCase {
 
-  protected $storage;
-  protected $event_dispatched;
-  protected $typed_config;
-  protected $event_dispatcher;
-  protected $config;
-
   /**
-   * __construct function
+   * Tests for class SettingsBase.
    */
-  public function __construct() {
-       parent::__construct();
-   }
+  public function testSettingsBase() {
+    $storage = $this->createMock(StorageInterface::class);
+    $event_dispatcher = $this->createMock(EventDispatcherInterface::class);
+    $typed_config = $this->createMock(TypedConfigManagerInterface::class);
 
-  /**
-   * {@inheritdoc}
-   */
+    $configs = $this->getMockBuilder(ImmutableConfig::class)
+      ->setConstructorArgs(['drupalConfig123',
+        $storage,
+        $event_dispatcher,
+        $typed_config,
+      ])
+      ->getMock();
 
-  public function setUp() {
-    parent::setUp();
-  }
-
-  /**
-   * tests for class SettingsBase
-   */
-
-  public function testSettingsBase () {
-    $this->storage = $this->createMock(StorageInterface::class);
-    $this->event_dispatcher = $this->createMock(EventDispatcherInterface::class);
-    $this->typed_config = $this->createMock(TypedConfigManagerInterface::class);
-    $this->configs = $this->getMockBuilder(ImmutableConfig::class)
-                          ->setConstructorArgs(array($this->config, $this->storage, $this->event_dispatcher, $this->typed_config))
-                          ->getMock();
     $collection = $this->getMockBuilder(SettingsBase::class)
-                       ->setConstructorArgs(array($this->configs))
-                       ->getMock();
+      ->setConstructorArgs([$configs])
+      ->getMock();
+
+    // Writing this assertion, otherwise the test will throw a warning. And this
+    // test is quite important as its checking for parent methods.
     $this->assertTrue($collection instanceof SettingsBase);
   }
 
-  /**
-   * tests for class SettingsInterface
-   */
-
-  public function testSettingsInterface () {
-    $collection = $this->createMock(SettingsInterface::class);
-    $this->assertTrue($collection instanceof SettingsInterface);
-  }
-
 }
-
- ?>
